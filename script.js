cf9de537e374dc840c24d4bbd057d903b682a78d
Johanan Galeon <johanangaleon@gmail.com>
2022-08-29 17:48:02 +0800

    Added basic text input and text writing

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let textStartX = 50;
+let textStartY = 50;
+
+//Text coordinates
+const textCoordsDisplay = document.querySelector('.text_coords_display');
+const setTextStart = (e) => {
+    textStartX = e.offsetX;
+    textStartY = e.offsetY;
+    textCoordsDisplay.innerHTML = `X: ${textStartX}, Y: ${textStartY}`
+}
+
+//Canvas
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
@@ -32,6 +44,7 @@ const drawImageToCanvas = (imageElement) => {
     canvas.width = imageElement.naturalWidth;
     canvas.height = imageElement.naturalHeight;
     ctx.drawImage(imageElement, 0,0);
+    canvas.style.transform = 'scale(.3)';
 }
 
 const imageInput = document.querySelector('#image_input');
@@ -40,5 +53,48 @@ imageInput.addEventListener('change', async (e) => {
     const image = await dataToImage(data);
     drawImageToCanvas(image);
 });
+
+canvas.addEventListener('click', setTextStart);
+
+//Writing text
+const writeText = (text) => {
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'black';
+    ctx.font = '50px Arial';
+    ctx.fillText (text, textStartX, textStartY);
+}
+
+const writeTextButton = document.querySelector('#write_text_button');
+const nameInput = document.querySelector('#name_input');
+writeTextButton.addEventListener('click', () => {
+    writeText(nameInput.value);
+})
+
+
+//Print handler
+const printCanvas = async () => {
+    let myWindow = window.open();
+    let image = await dataToImage(canvas.toDataURL());
+    await myWindow.document.body.appendChild(image);
+    myWindow.focus();
+    myWindow.print();
+    myWindow.close();
+}
+
+const printButton = document.querySelector('#print_button');
+printButton.addEventListener('click', printCanvas)
+
+
+
+
+const init = () => {
+    const image = new Image();
+    image.src = './image/dummy.png';
+    image.onload = () => {
+        drawImageToCanvas(image);
+    }
+}
+
+init();
 
 
