38fbe14944948c5263dcba4c54135b713793f6da
Johanan Galeon <johanangaleon@gmail.com>
2022-08-30 19:39:35 +0800

    Added more basic input. Can now set starting and ending point for text from multiple input fields.

--- script.js
+++ script.js
@@ -1,13 +1,87 @@
-let textStartX = 50;
-let textStartY = 50;
+const SCALE_FACTOR = 0.3; 
+
+let textData = {
+    name: {
+        text: '',
+        x: 0,
+        y: 0,
+        width: 0,
+    },
+    date: {
+        text: '',
+        x: 0,
+        y: 0,
+        width: 0,
+    },
+    address: {
+        text: '',
+        x: 0,
+        y: 0,
+        width: 0,
+    },
+    sex: {
+        text: '',
+        x: 0,
+        y: 0,
+        width: 0,
+    },
+    age: {
+        text: '',
+        x: 0,
+        y: 0,
+        width: 0,
+    }
+}
+//Text input handler
+const form = document.querySelector('#form');
+const submitData = (e) => {
+    e.preventDefault();
+    const dataElements = [...e.target.elements].filter(element => {
+        const classes = [...element.classList]
+        return classes.includes('input_data')
+    });
+    dataElements.forEach(element => {
+        // console.log(element.value)
+        textData[element.dataset.name].text = element.value;
+    });
+    // console.log(textData);
+}
+
+form.addEventListener('submit', submitData);
 
 //Text coordinates
+const setCoordsButtons = document.querySelectorAll('.button_set_coords');
 const textCoordsDisplay = document.querySelector('.text_coords_display');
-const setTextStart = (e) => {
-    textStartX = e.offsetX;
-    textStartY = e.offsetY;
-    textCoordsDisplay.innerHTML = `X: ${textStartX}, Y: ${textStartY}`
+
+let target = '';
+let setActive = false;
+let setWidth = false;
+
+const setTarget = (e) => {
+    target = e.target.dataset.name;
+    setActive = true;
+    setWidth = false;
+    console.log(`setActive: ${setActive},`, `setWidth: ${setWidth},`, `target: ${target}`)
 }
+
+const setCoords = (e) => {
+    if (!setActive) return;
+    if (!setWidth) {
+        textData[target].x = e.offsetX;
+        textData[target].y = e.offsetY;
+        setWidth = true;
+        textCoordsDisplay.innerHTML = `X: ${textData[target].x}, Y: ${textData[target].y} W: ${textData[target].width}`;
+    } else {
+        textData[target].width = e.offsetX - textData[target].x;
+        setWidth = false;
+        setActive = false;
+        textCoordsDisplay.innerHTML = `X: ${textData[target].x}, Y: ${textData[target].y} W: ${textData[target].width}`;
+    }
+}
+
+setCoordsButtons.forEach(button => {
+    button.addEventListener('click', setTarget);
+})
 
 //Canvas
 const canvas = document.querySelector('#canvas');
@@ -44,31 +118,42 @@ const drawImageToCanvas = (imageElement) => {
     canvas.width = imageElement.naturalWidth;
     canvas.height = imageElement.naturalHeight;
     ctx.drawImage(imageElement, 0,0);
-    canvas.style.transform = 'scale(.3)';
+    canvas.style.transform = `scale(${SCALE_FACTOR})`;
 }
 
-const imageInput = document.querySelector('#image_input');
+const imageInput = document.querySelector('#input_image');
 imageInput.addEventListener('change', async (e) => {
     const data = await readFile(e.target.files[0]);
     const image = await dataToImage(data);
     drawImageToCanvas(image);
 });
 
-canvas.addEventListener('click', setTextStart);
+canvas.addEventListener('click', setCoords);
 
 //Writing text
-const writeText = (text) => {
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle = 'black';
-    ctx.font = '50px Arial';
-    ctx.fillText (text, textStartX, textStartY);
+const writeText = (x, y, width, text, size, color) => {
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.textBaseline = 'middle';
+    ctx.font = `${size / SCALE_FACTOR}px Arial`;
+    ctx.fillText (text, x, y, width);
 }
 
-const writeTextButton = document.querySelector('#write_text_button');
+const writeAllTextData = () => {
+    const keys = Object.keys(textData);
+    for (let i = 0; i < keys.length; i++) {
+        const {x, y, width, text} = textData[keys[i]];
+        console.log(x, y, width, text)
+        writeText(x, y, width, text, 15, 'black');
+    }
+    // textData.forEach(data => {
+    //     
+    // })
+}
+
+const writeButton = document.querySelector('#button_write');
 const nameInput = document.querySelector('#name_input');
-writeTextButton.addEventListener('click', () => {
-    writeText(nameInput.value);
-})
+writeButton.addEventListener('click', writeAllTextData);
 
 
 //Print handler
@@ -81,7 +166,7 @@ const printCanvas = async () => {
     myWindow.close();
 }
 
-const printButton = document.querySelector('#print_button');
+const printButton = document.querySelector('#button_print');
 printButton.addEventListener('click', printCanvas)
 
 
