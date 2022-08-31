fdb7e709504060d0624f4953ba9541c76c57249a
Johanan Galeon <johanangaleon@gmail.com>
2022-08-31 18:13:21 +0800

    Marker dragging and resizing prototype now working

--- marker.js
+++ marker.js
@@ -1,16 +1,16 @@
 const container = document.querySelector('#container');
-const marker1 = document.querySelector('#marker1');
 
-let active = false;
+//Marker dragging
+let dragActive = false;
 let selectedMarker = null;
 let markerOffsetX, markerOffsetY;
-let currentX, currentY;
+let dragCurrentX, dragCurrentY;
 
 const dragStart = (e) => {
     const isMarker = [...e.target.classList].includes('marker');
     if (isMarker && !selectedMarker) {
-        selectedMarker = e.target
-        active = true;
+        selectedMarker = e.target;
+        dragActive = true;
         markerOffsetX = e.offsetX;
         markerOffsetY = e.offsetY;
     }
@@ -18,23 +18,74 @@ const dragStart = (e) => {
 }
 
 const drag = (e) => {
-    if (!active) return;
+    if (!dragActive) return;
 
-    currentX = e.clientX - container.offsetLeft - markerOffsetX;
-    currentY = e.clientY - container.offsetTop - markerOffsetY;
+    dragCurrentX = e.clientX - container.offsetLeft - markerOffsetX;
+    dragCurrentY = e.clientY - container.offsetTop - markerOffsetY;
 
-    if (currentX > container.offsetWidth - selectedMarker.offsetWidth || currentX < 0) return;
-    if (currentY > container.offsetHeight - selectedMarker.offsetWidth || currentY < 0) return;
+    if (dragCurrentX > container.offsetWidth - selectedMarker.offsetWidth || dragCurrentX < 0) return;
+    if (dragCurrentY > container.offsetHeight - selectedMarker.offsetWidth || dragCurrentY < 0) return;
 
-    selectedMarker.style.left = `${currentX}px`;
-    selectedMarker.style.top = `${currentY}px`;
+    selectedMarker.style.top = `${dragCurrentY}px`;
+    selectedMarker.style.left = `${dragCurrentX}px`;
 }
 
 const dragEnd = (e) => {
-    active = false;
+    dragActive = false;
     selectedMarker = null;
 }
 
 container.addEventListener('mousedown', dragStart);
 container.addEventListener('mousemove', drag);
 container.addEventListener('mouseup', dragEnd);
+
+//Marker resizing
+const resizeButtons = document.querySelectorAll('.marker_resize');
+
+
+let resizeActive = false;
+let selectedResizeMarker = null;
+let resizeInitialX, resizeInitialY;
+
+let addedWidth, addedHeight;
+
+
+const resizeStart = (e) => {
+    const isResize = [...e.target.classList].includes('marker_resize');
+    if (isResize && !selectedResizeMarker) {
+        selectedResizeMarker = e.target.parentNode;
+        resizeActive = true;
+        resizeInitialX = e.clientX;
+        resizeInitialY = e.clientY;
+    }
+}
+
+const resize = (e) => {
+    if (resizeActive) {
+        // console.log('resizing')
+        newWidth = selectedResizeMarker.offsetWidth + e.clientX - resizeInitialX;
+        newHeight = selectedResizeMarker.offsetHeight + e.clientY - resizeInitialY;
+
+        selectedResizeMarker.style.width = `${newWidth}px`;
+        selectedResizeMarker.style.height = `${newHeight}px`;
+
+        resizeInitialX = e.clientX;
+        resizeInitialY = e.clientY;
+    }
+}
+
+const resizeEnd = (e) => {
+    resizeActive = false;
+    selectedResizeMarker = null;
+}
+
+// resizeButtons.forEach(button => {
+//     button.addEventListener('mousedown', resizeStart);
+//     button.addEventListener('mousemove', resize);
+//     button.addEventListener('mouseup', resizeEnd);
+
+// })
+
+    container.addEventListener('mousedown', resizeStart);
+    container.addEventListener('mousemove', resize);
+    container.addEventListener('mouseup', resizeEnd);
